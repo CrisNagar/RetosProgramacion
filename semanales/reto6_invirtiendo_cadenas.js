98bdf25c38f235c01c56e48e9a9ed21537186a6a
CrisNagar <crisnagardev@gmail.com>
2022-08-24 12:57:02 +0200

    Solución reto #6 javaScript

---
+++ semanales/reto6_invirtiendo_cadenas.js
@@ -0,0 +1,31 @@
+/*
+ * Reto #6
+ * INVIRTIENDO CADENAS
+ * Fecha publicación enunciado: 07/02/22
+ * Fecha publicación resolución: 14/02/22
+ * Dificultad: FÁCIL
+ *
+ * Enunciado: Crea un programa que invierta el orden de una cadena de texto sin usar funciones propias del lenguaje que lo hagan de forma automática.
+ * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+
+function reverseString(string) {
+    if (typeof string !== 'string') return 'Debe ser una cadena de texto';
+
+    const splitString = string.split('');
+    let tempString = '';
+    for(let i = splitString.length; i >= 0; i--){
+        tempString += splitString[i];
+    }
+
+    return tempString;
+}
+
+console.log(reverseString("Hola! mundo."));
