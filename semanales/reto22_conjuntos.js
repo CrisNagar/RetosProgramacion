b4647007eae6c44fc7485a081226496815d3a881
CrisNagar <crisnagardev@gmail.com>
2022-08-25 14:03:28 +0200

    Solución reto #22 JavaScript

---
+++ semanales/reto22_conjuntos.js
@@ -0,0 +1,54 @@
+/*
+ * Reto #22
+ * CONJUNTOS
+ * Fecha publicación enunciado: 01/06/22
+ * Fecha publicación resolución: 07/06/22
+ * Dificultad: FÁCIL
+ *
+ * Enunciado: Crea una función que reciba dos array, un booleano y retorne un array.
+ * - Si el booleano es verdadero buscará y retornará los elementos comunes de los dos array.
+ * - Si el booleano es falso buscará y retornará los elementos no comunes de los dos array.
+ * - No se pueden utilizar operaciones del lenguaje que lo resuelvan directamente.
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la comunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+
+function getSet(array1, array2, comunes) {
+    let output = [];
+
+    if (comunes) {
+        array1.forEach(el => {
+            if (array2.includes(el) && !output.includes(el)) {
+                output.push(el);
+            }
+        });
+
+        array2.forEach(el => {
+            if (array1.includes(el) && !output.includes(el)) {
+                output.push(el);
+            }
+        });
+    } else {
+        array1.forEach(el => {
+            if (!array2.includes(el) && !output.includes(el)) {
+                output.push(el);
+            }
+        });
+
+        array2.forEach(el => {
+            if (!array1.includes(el) && !output.includes(el)) {
+                output.push(el);
+            }
+        });
+    }
+
+    return output;
+}
+
+console.log(getSet([1, 2, 3, 3, 4], [2, 2, 3, 3, 3, 4, 6], true))
+console.log(getSet([1, 2, 3, 3, 4], [2, 2, 3, 3, 3, 4, 6], false))
